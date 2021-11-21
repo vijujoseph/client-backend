b531ff4b3729aaaec618d81365b85fd4282574a6
vijujoseph@gmail.com <Iloveak@47>
2021-11-21 15:56:15 +0100

    few domain correction commit

--- src/main/resources/db/migration/V1__createInitialTables.sql
+++ src/main/resources/db/migration/V1__createInitialTables.sql
@@ -1,3 +1,10 @@
+CREATE TABLE ROUTES (
+                        route_id bigint(20) NOT NULL AUTO_INCREMENT,
+                        route_name varchar(20),
+                        PRIMARY KEY (route_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert into ROUTES values (300, 'smart city route');
+
 CREATE TABLE BOATS (
                        boat_id bigint(20) NOT NULL AUTO_INCREMENT,
                        capacity int NOT NULL,
@@ -22,6 +29,16 @@ CREATE TABLE CALENDER (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into calender values (800, false, true, false, false, false, false, false, now(), now());
 
+CREATE TABLE TRIPS (
+                       trip_id bigint(20) NOT NULL AUTO_INCREMENT,
+                       route_id bigint(20) NOT NULL,
+                       service_id bigint(20) NOT NULL,
+                       trip_headsign varchar(20),
+                       foreign key (route_id) references ROUTES(route_id),
+                       foreign key (service_id) references CALENDER(service_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert into trips values (2000, 300,800, 'trip_headsign');
+
 CREATE TABLE STOPS (
                        stop_id bigint(20) NOT NULL AUTO_INCREMENT,
                        stop_name varchar(50) NOT NULL,
@@ -35,21 +52,16 @@ insert into STOPS values (100, 'Ernakulam South', '1.0000e32', '3.232d22', 1, tr
 insert into STOPS values (101, 'Smart City', '2.1320e32', '1.0232d22', 1, true);
 
 CREATE TABLE TRIP_INVENTORY (
-  trip_id bigint(20) NOT NULL,
+  id bigint(20) NOT NULL,
   journey_date TIMESTAMP WITH TIME ZONE NOT NULL,
   stop_id bigint(20) NOT NULL,
+  trip_id bigint(20) NOT NULL,
   stop_sequence int,
-  PRIMARY KEY (trip_id),
-  foreign key (stop_id) references STOPS(stop_id)
+  PRIMARY KEY (id),
+  foreign key (stop_id) references STOPS(stop_id),
+  foreign key (trip_id) references TRIPS(trip_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into TRIP_INVENTORY values (500, now(), 100, 3);
-
-CREATE TABLE ROUTES (
-                        route_id bigint(20) NOT NULL AUTO_INCREMENT,
-                        route_name varchar(20),
-                        PRIMARY KEY (route_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into ROUTES values (300, 'smart city route');
+insert into TRIP_INVENTORY values (500, now(), 100, 2000, 3);
 
 CREATE TABLE STOP_TIMES (
   id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -58,9 +70,11 @@ CREATE TABLE STOP_TIMES (
   departure_time  TIMESTAMP WITH TIME ZONE NOT NULL,
   stop_id bigint(20) NOT NULL,
   stop_sequence int,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  foreign key (trip_id) references TRIPS(trip_id),
+  foreign key (stop_id) references STOPS(stop_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into STOP_TIMES  values (1, 1, now(), now(), 1, 3);
+insert into STOP_TIMES  values (1, 2000, now(), now(), 101, 3);
 
 CREATE TABLE STOPS_IN_ROUTE (
   id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -93,19 +107,6 @@ CREATE TABLE FARE_ATTRIBUTES (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into FARE_ATTRIBUTES values (1, 200, 15, 'INR');
 
-CREATE TABLE TRIPS (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  route_id bigint(20) NOT NULL,
-  service_id bigint(20) NOT NULL,
-  trip_id bigint(20) NOT NULL,
-  trip_headsign varchar(20),
-  PRIMARY KEY (id),
-  foreign key (route_id) references ROUTES(route_id),
-  foreign key (trip_id) references TRIP_INVENTORY(trip_id),
-  foreign key (service_id) references CALENDER(service_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into trips values (600, 300,800, 500, 'trip_headsign');
-
 CREATE TABLE SCHEDULE (
                           schedule_id bigint(20) NOT NULL AUTO_INCREMENT,
                           start_date TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -123,7 +124,7 @@ CREATE TABLE TRIPS_IN_SCHEDULE (
   foreign key (trip_id) references TRIP_INVENTORY(trip_id),
   foreign key (schedule_id) references SCHEDULE(schedule_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into TRIPS_IN_SCHEDULE values (1, 900,500);
+insert into TRIPS_IN_SCHEDULE values (1, 900,2000);
 
 CREATE TABLE SCHEDULED_JOURNEY (
   id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -140,8 +141,8 @@ CREATE TABLE SALES_RECORDS (
   order_id bigint(20) NOT NULL AUTO_INCREMENT,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL,
   created_by varchar(20),
-  origin_jetty varchar(20),
-  destination_jetty varchar(20),
+  origin_jetty bigint(20) NOT NULL,
+  destination_jetty bigint(20) NOT NULL,
   number_of_tickets int,
   amount_paid bigint(20) NOT NULL,
   ticket_fare bigint(20) NOT NULL,
@@ -151,9 +152,12 @@ CREATE TABLE SALES_RECORDS (
   date_of_journey  TIMESTAMP WITH TIME ZONE NOT NULL,
   status varchar,
   signature varchar,
-  PRIMARY KEY (order_id)
+  PRIMARY KEY (order_id),
+  foreign key (origin_jetty) references STOPS(stop_id),
+  foreign key (destination_jetty ) references STOPS(stop_id),
+  foreign key (schedule_id) references SCHEDULE(schedule_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-insert into SALES_RECORDS  values (1001, now(), 'Vibin Raj', 'EKM', 'NYD', 2, 100, 100, 1, 1, 1, now(), 'SUCCESS', 'signature');
+insert into SALES_RECORDS  values (1001, now(), 'Vibin Raj', 100, 101, 2, 100, 100, 900, 1, 1, now(), 'SUCCESS', 'signature');
 
 CREATE TABLE PAYMENT_DETAILS     (
     id bigint(20) NOT NULL AUTO_INCREMENT,
